55ab0bf9750622ec39dcf750b71728c89cf2e0f8
jhon <jhonmarcosaraujo@yahoo.com>
2023-10-01 11:40:05 -0400

    replace the relief off button

--- interface.py
+++ interface.py
@@ -1,19 +1,14 @@
 from tkinter import *
 from fuctions import *
 
-
-
-
 class ui:
 
     def __init__(self,master):
-        
-
 
         self.scrollbar = Scrollbar(master)
         self.scrollbar.pack(side= RIGHT,fill = Y)
 
-        self.caja_texto = Text(master,borderwidth=5,padx=10,pady=0,font=60,yscrollcommand=self.scrollbar.set)
+        self.caja_texto = Text(master,borderwidth=5,padx=10,pady=0,font=60,yscrollcommand=self.scrollbar.set,relief="ridge")
        
         self.caja_texto.config(font=('Arial', 16))
         self.caja_texto.place(x=10,y=30)
@@ -23,20 +18,20 @@ class ui:
         
 
 
-        self.boton_guardar = Button(master,text="Guardar",command = lambda:guardar(self.caja_texto))
+        self.boton_guardar = Button(master,text="Guardar",command = lambda:guardar(self.caja_texto),relief="ridge")
         self.boton_guardar.place(x=10,y=1)
 
-        self.boton_personalizar = Button(master,text="Personalizar fondo",command=lambda:personalizar_fondo(master,self.caja_texto))
+        self.boton_personalizar = Button(master,text="Personalizar fondo",command=lambda:personalizar_fondo(master,self.caja_texto),relief="ridge")
         self.boton_personalizar.place(x=60,y=1)
 
-        self.boton_oscuro = Button(master,text="Noche",command=lambda :oscuro(self.caja_texto,master))
+        self.boton_oscuro = Button(master,text="Noche",command=lambda :oscuro(self.caja_texto,master),relief="ridge")
         self.boton_oscuro.place(x=130,y=1)
 
-        self.boton_claro =Button(master,text = "claro",command=lambda:modo_claro(self.caja_texto,master))
-        self.boton_claro.place(x=170,y=1)
+        self.boton_claro =Button(master,text = "claro",command=lambda:modo_claro(self.caja_texto,master),relief="ridge")
+        self.boton_claro.place(x=175,y=1)
 
 
-        self.open_button = Button(master,text="Abrir",command=lambda:abrir_archivo(self.caja_texto))
+        self.open_button = Button(master,text="Abrir",command=lambda:abrir_archivo(self.caja_texto),relief="ridge")
         self.open_button.place(x=210,y=1)
         
 #-----------------------------------------------------------------------------------------instancias---------------------
@@ -49,7 +44,7 @@ try:
     root = Tk()
     root.title("Pynotepad")
     root.config(bg="white")
-    root.geometry("1089x600")
+    root.geometry("1030x620")
     ar = ui(root)
 except:
     root.showerror("invalido","hubo un error")
